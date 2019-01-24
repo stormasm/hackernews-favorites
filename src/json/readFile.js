5d457a6de96e5fb04d155d52f7afe61f697fe6e5
Michael Angerman <stormasm@gmail.com>
2019-01-24 10:55:44 -0800

    writing data to the redis favoriteset

--- src/json/readFile.js
+++ src/json/readFile.js
@@ -2,17 +2,31 @@
 import favids from "./../../data/in/fvid.json";
 import { writeJsonDataToFilename } from '../util/file-util'
 
+import { sadd } from "./../redis/writeUtils";
+
 const fn = 'test'
+const keyname = 'favoriteset'
 
-async function readFavorites() {
-
+async function readFavoritesFileTest() {
   let json = JSON.stringify(favids)
   let filename = './data/out/' + fn + '.json'
   await writeJsonDataToFilename(filename, json)
 }
 
+async function readFavoritesFileTest() {
+  let json = JSON.stringify(favids)
+  let filename = './data/out/' + fn + '.json'
+  await writeJsonDataToFilename(filename, json)
+}
+
+function processIdAry(ids) {
+  ids.forEach(function(item) {
+    sadd(keyname, item);
+  });
+}
+
 async function go() {
-  await readFavorites()
+  await processIdAry(favids)
 }
 
 go()
